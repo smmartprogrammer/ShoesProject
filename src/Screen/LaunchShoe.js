8e46d7b2d55b8ca6de068374fe98d28cc410ae97
Raghunathji <partabrajp@gmail.com>
2021-06-15 22:21:30 +0500

    full web completed

--- src/Screen/LaunchShoe.js
+++ src/Screen/LaunchShoe.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Shoes from '../shoes.json';
 import { useParams } from 'react-router';
+import './LaunchShoe.css';
 
 const LaunchShoe = () => {
   const { id } = useParams();
@@ -11,11 +12,43 @@ const LaunchShoe = () => {
   }
 
   const { name, img, price } = shoe;
+
   return (
-    <div>
-      <h2>{name} </h2>
-      <img src={img} alt={name} />
-      <h2>{price} </h2>
+    <div className="productscreen">
+      <div className="productscreen_left">
+        <div className="left__image">
+          <img src={img} alt="product image" />
+        </div>
+        <div className="left__info">
+          <p className="left__name">{name}</p>
+          <p>
+            Price: <span>{price}</span>
+          </p>
+          <p>{id}</p>
+        </div>
+      </div>
+      <div className="productscreen_right">
+        <div className="right__info">
+          <p>
+            Price: <span>{price}</span>
+          </p>
+          <p>
+            Status: <span>In Stock</span>
+          </p>
+          <p>
+            Qty{' '}
+            <select>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+            </select>
+          </p>
+          <p>
+            <button type="button">Add to Cart</button>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
